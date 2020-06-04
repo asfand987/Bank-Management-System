959c5e32a300c629f874e91fa0325f92a6609023
asfand987 <asfand987.ak@gmail.com>
2020-06-04 21:40:51 +0100

    Small change to Create_Account()

--- Bank-Management-System.cpp
+++ Bank-Management-System.cpp
@@ -13,26 +13,43 @@ public:
     void create_account();              //create new account.
 };
 
+
+void create_account();
+
 int main() {
-    //Main menu 
+    //Main menu
 
-    cout << "---------- MAIN MENU ----------" << endl;
-    cout << "           01. NEW ACCOUNT" << endl;
-    cout << "           02. DEPOSIT" << endl;
-    cout << "           03. WITHDRAW" << endl;
-    cout << "           04. BALANCE ENQUIRY" << endl;
-    cout << "           05. ALL ACCOUNT HOLDER LIST" << endl;
-    cout << "           06. CLOSE AN ACCOUNT" << endl;
-    cout << "           07. MODIFY AND ACCOUNT" << endl;
-    cout << "           08. EXIT" << endl;
-    cout << "-------------------------------" << endl;
-
-    //User inputs
     int i;
-    cout << "Select Your Option <1-8>: ";  
-    cin >> i;
+    do {
+        cout << "---------- MAIN MENU ----------" << endl;
+        cout << "           01. NEW ACCOUNT" << endl;
+        cout << "           02. DEPOSIT" << endl;
+        cout << "           03. WITHDRAW" << endl;
+        cout << "           04. BALANCE ENQUIRY" << endl;
+        cout << "           05. ALL ACCOUNT HOLDER LIST" << endl;
+        cout << "           06. CLOSE AN ACCOUNT" << endl;
+        cout << "           07. MODIFY AND ACCOUNT" << endl;
+        cout << "           08. EXIT" << endl;
+        cout << "-------------------------------" << endl;
+
+        //User inputs
+        cout << "Select Your Option <1-8>: ";  
+        cin >> i;
+        
+        switch(i) {
+            case '1' :
+                create_account();
+                break;
+            default:
+                cout << "INCORRECT INPUT" << endl;
+        }
+
+    } while(i != '8'); {
+        return 0;
+    }
 }
 
 void account::create_account() {
-    
+    cout << "YOU SELECTED 01" << endl;
+    cout << "CREATE NEW ACCOUNT" << endl;
 }
