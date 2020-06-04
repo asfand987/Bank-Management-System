db91817e1ac9b9bc22c7806ae1b8dc11254d3f83
asfand987 <asfand987.ak@gmail.com>
2020-06-04 20:42:47 +0100

    Added main user GUI

---
+++ Bank-Management-System.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //Main menu 
+
+    cout << "---------- MAIN MENU ----------" << endl;
+    cout << "           01. NEW ACCOUNT" << endl;
+    cout << "           02. DEPOSIT" << endl;
+    cout << "           03. WITHDRAW" << endl;
+    cout << "           04. BALANCE ENQUIRY" << endl;
+    cout << "           05. ALL ACCOUNT HOLDER LIST" << endl;
+    cout << "           06. CLOSE AN ACCOUNT" << endl;
+    cout << "           07. MODIFY AND ACCOUNT" << endl;
+    cout << "           08. EXIT" << endl;
+    cout << "-------------------------------" << endl;
+
+
+}
