a57cca9901f7ced09f5b192659efd73a669cc612
asfand987 <asfand987.ak@gmail.com>
2020-06-05 19:33:43 +0100

    Added create new Account functionality

--- Bank-Management-System.cpp
+++ Bank-Management-System.cpp
@@ -11,25 +11,47 @@ class account {
 
 
 public:
-    void create_account();              //create new account.
+    void create_account(); 
+    void deposit();             
+    //3
+    void balance_enquiry();
 };
 
-void account::create_account() {
-    cout << "Testing" << endl;
+void account::create_account()
+{
+	cout  <<  "\n Enter The account No. :";
+	cin >> accountNumber;
+	
+    cout << "\n\n Enter The Name of The account Holder : ";
+	cin.ignore();
+	cin.getline(actHolderName,50);
+	
+    cout << "\n Enter Type of The account (C/S) : ";
+	cin >> type;
+	type = toupper(type);
+	
+    cout << "\n Enter The Initial amount(>=500 for Saving and >=1000 for current ) : ";
+	cin >> deposit;
+	cout << "\n\n\n Account Created......Press Any Key To Continue..." << endl;
 }
 
 
-void write();
+}
+
+
+void write_account();
+void intro();
 
 int main() {
     //Main menu
 
     char ch;
 	int num;
-	//intro();
+	intro();
+
 	do
 	{
-		system("cls");
+		//system("cls");
 		cout << "---------- MAIN MENU ----------" << endl;
         cout << "           01. NEW ACCOUNT" << endl;
         cout << "           02. DEPOSIT" << endl;
@@ -43,15 +65,22 @@ int main() {
 
         //User inputs
         cout << "Select Your Option <1-8>: ";  
-        cin >> num;
+        cin >> ch;
         
-		system("cls");
+		//system("cls");
 		switch(ch)
 		{
 		case '1':
-			write();
+			write_account();
 			break;
-		 default :cout<<"\a";
+        case '2': 
+        case '3': 
+        case '4': 
+        case '5': 
+        case '6': 
+        case '7': 
+        case '8':   
+		default : cout<<"\a";
 		}
 		cin.ignore();
 		cin.get();
@@ -60,15 +89,31 @@ int main() {
 }
 
 
-void write() {
+void write_account() {
+    account ac;
+	ofstream outFile;
+	outFile.open("account.dat",ios::binary|ios::app);
+	ac.create_account();
+	outFile.write(reinterpret_cast<char *> (&ac), sizeof(account));
+	outFile.close();
+
+    /*cout << "Testing :" << endl;
     account ac;
 	ofstream outFile;
 	outFile.open("list.txt",ios::binary|ios::app);
 	ac.create_account();
 	outFile.write(reinterpret_cast<char *> (&ac), sizeof(account));
-	outFile.close();
+	outFile.close();*/
 }
 
-void intro() {
 
+void intro()
+{
+	cout << "\n\n\n\t  BANK";
+	cout << "\n\n\tMANAGEMENT";
+	cout << "\n\n\t  SYSTEM";
+	cout << "\n\n\n\nMADE BY : ASFAND KHAN";
+	cout << "\n\nPRESS ENTER TO CONTINUE: ";
+    cout << endl;
+	cin.get();
 }
