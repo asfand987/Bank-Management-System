6142a740621d425415b9d0f7a8b0e389e2b01f76
asfand987 <asfand987.ak@gmail.com>
2020-06-10 19:31:54 +0100

    Cleaned up code and added appropriate comments

--- Bank-Management-System.cpp
+++ Bank-Management-System.cpp
@@ -6,12 +6,16 @@
 
 using namespace std;
 
+/***
+ * Banking-Management-System.
+***/
+
 class account {
 
-    int id; //change to id
+    int id; 
     char actHolderName[50];
     int balance;
-    char type;   //checking or savings account.
+    char type;   
 
 public:
 
@@ -40,42 +44,53 @@ account(int id, char actHolderName, int balance, char type) {
 };
 
 //------------------------------------------------------------------------------------------------------------------------------
+// Functions with access to Account.
 
 /***    
  * Function which displays the interface to the user who can then input the required values in order to make a new account.
+ * Function is called by @write_account() method.
 ***/
 void account::create_account()  {
 
-	cout  <<  "\n Enter The account No. :";
+	cout  <<  "\n       Enter The account No. :";
 	cin >> id;
 	
-    cout << "\n\n Enter The Name of The account Holder : ";
+    cout << "\n\n       Enter The Name of The account Holder : ";
 	cin.ignore();
 	cin.getline(actHolderName,50);
 	
-    cout << "\n Enter Type of The account (c/s) : ";
+    cout << "\n         Enter Type of The account (c/s) : ";
 	cin >> type;
 
     if((type != 'c') && (type != 's')) {
-        cout << "Incorrect Account Type Enterred, Please Enter Correct Type (c/s) : ";
+        cout << "       Incorrect Account Type Enterred, Please Enter Correct Type (c/s) : ";
         cin >> type;
     }
 
-    cout << "\n Enter Initial Deposit Amount: ";
+    cout << "\n         Enter Initial Deposit Amount: ";
     cin >> balance;
 
-    cout << "\n Deposit Amount: " << balance << " Successful." << endl;
-    cout << "\n Press Any Key To Continue...";
+    cout << "\n         Deposit Amount: " << balance << " Successful." << endl;
+    cout << "\n         Press Any Key To Continue...";
 
 }
 
+/***
+ * Returns the id of the account.
+***/
 int account::return_account() const {
+
     return id;
 }
 
+/***
+ * Function which displays all the information about the account. 
+ * Function is called by @display_actInfo(int) which is used in @main.
+***/
 void account::show_account() const {
+
     cout << "--------------------------------" << endl;
-	cout << "        BALANCE ENQUIRY" << endl;
+	cout << "        ACCOUNT ENQUIRY" << endl;
     cout << endl;
     cout << "        Account ID: " << id << endl;
     cout << "        Account Holder Name: " << actHolderName << endl;
@@ -83,26 +98,32 @@ void account::show_account() const {
     cout << endl;
     cout << "        BALANCE: " << balance << endl;
     cout << "--------------------------------" << endl;
+
 }
 
+/***
+ * Function deposits amount that the user entered into the account.
+ * Function is called by @deposit_amount(int) which is used in @main.
+***/
 void account::depo(int n) {
-    //cout << deposit << "1 " << endl;
+
     balance = balance + n;
-    cout << "Amount Successfully Deposited, New Balance : " << balance << endl;
-    cout << "Press Any Key To Continue...";
-    //cout << deposit << "2 " << endl;
-    //cout<<"\n\n\t Record Updated, Press Any Key To Continue...";
+    cout << "     Amount Successfully Deposited, New Balance : " << balance << endl;
+    cout << "     Press Any Key To Continue...";
+    
 };
 
+/***
+ * Function withdraws money from the account.
+ * Function is called by @withdraw_amount(int) which is used in @main.
+***/
 void account::withdraw(int n) {
-    //int temp = deposit;
+    
     int &checkValue = balance;
     checkValue = checkValue - n;
-    //deposit = deposit - n;
 
     if(checkValue < 0) {
-        //deposit = temp;
-        //cout << temp << " " << checkValue;
+        
         checkValue = checkValue + n;
         cout << "      Withdrawal Failed Due To Insufficient Funds, Press Any Key To Continue...";
     }
@@ -113,6 +134,10 @@ void account::withdraw(int n) {
     }
 };
 
+/***
+ * Function which allows account holder to modify details about the account owned.
+ * Function is used in @modify_account() which is used in @main.
+***/
 void account::modify() {
     char ch;
     
@@ -138,33 +163,88 @@ void account::modify() {
         cin >> actHolderName;
         cout << "Account Holder Name Successully Changed. Press Any Key To Continue...";
     }
+    else {
+        cout <<"Press Any Key To Continue..." << endl;
+    }
     
 };
 
+/***
+ * Function displays a list of all accounts currently in the database.
+ * Function is called by @display_all() which is used in @main.
+***/
 void account::display_accounts() {
-    cout << "--------------------------------" << endl;
-    cout << "ACCOUNTS LIST" << endl;
-    cout << "Account ID: " << id << endl;
-    cout << "Account Holder Name: " << actHolderName << endl;
-    cout << "Account Type: " << type << endl;
-    cout << "--------------------------------" << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << "               ACCOUNTS LIST:" << endl;
+    cout << "       Account ID: " << id << endl;
+    cout << "       Account Holder Name: " << actHolderName << endl;
+    cout << "       Account Type: " << type << endl;
+    cout << "--------------------------------------------" << endl;
     cout << endl;
 }   
 
 
-//----------------------------------------------------------------------
-
-void write_account();
-void intro();
-void display_balance(int);
-void deposit_amount(int);
-void withdraw_amount(int);
-void close_account();
-void modify_account();
-void transfer_money();
-void display_all();
+//-----------------------------------------------------------------------------------------------------------------------
+//Functions used in @Main().
 
 /***
+ * Information user sees upon starting the program.
+***/
+void intro();
+
+/***
+ * 01. 
+***/
+void write_account();
+
+/***
+ * 02. 
+***/
+void deposit_amount(int);
+
+/***
+ * 03. 
+***/
+void withdraw_amount(int);
+
+/***
+ * 04. 
+***/
+void display_actInfo(int);
+
+/***
+ * 05. 
+***/
+void close_account();
+
+/***
+ * 06. 
+***/
+void modify_account();
+
+/***
+ * 07. 
+***/
+void display_all();
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+/***
+ * Function used to display information at the start of the program.
+***/
+void intro()
+{
+	cout << "\n\n\n\t  BANK";
+	cout << "\n\n\tMANAGEMENT";
+	cout << "\n\n\t  SYSTEM";
+	cout << "\n\n\n\nMADE BY : ASFAND KHAN";
+	cout << "\n\nPRESS ENTER TO CONTINUE: ";
+    cout << endl;
+	cin.get();
+}
+
+/***
+ * 01.
  * Function which wrties the data that the user entered in @create_account() into the "account.dat" file.
 ***/
 void write_account() {
@@ -175,76 +255,103 @@ void write_account() {
 	ac.create_account();
 
 	outFile.write(reinterpret_cast<char *> (&ac), sizeof(account));
+
 	outFile.close();
 }
 
-//2
+/***
+ * 02.
+ * Function used to deposit money into the account. 
+ * @depo(int) function used from Account class.
+***/
 void deposit_amount(int n) {
     account ac;
     int amount;
     fstream File;
+
     File.open("account.dat", ios::binary|ios::in|ios::out);
     bool found = false;
 
     while(!File.eof()) {
+
         File.read(reinterpret_cast<char *> (&ac), sizeof(account));
+
         if((ac.return_account() == n) && (n > 0)) {
-            cout << "Enter Deposit Amount: ";
+
+            cout << "       Enter Deposit Amount: " << endl;
             cin >> amount;
+
             ac.depo(amount);
 
+            //sets the position where the next character is to be inserted to prevent copys.
             int pos=(-1)*static_cast<int>(sizeof(ac));
-			File.seekp(pos,ios::cur);       //sets the position where the next character is to be inserted to prevent copys.
+			File.seekp(pos,ios::cur);   
+
             found = true;
             File.write(reinterpret_cast<char *> (&ac), sizeof(account));
-	    	//cout<<"\n\n\t Record Updated, Press Any Key To Continue...";
+	    	
             break;
         }
-
-			//found=true;
     }
 
     File.close();
-    if(found == false) {
+
+    if(!found) {
         cout << "Record Does Not Exist... Press Any Key To Continue...";
     }
 }
 
-//3
+/***
+ * 03.
+ * Function used to withdraw money out of the account. 
+ * @withdraw(int) function used from Account class.
+***/
 void withdraw_amount(int n) {
     account ac; 
     int withdraw_amnt;
     fstream File;
+
     File.open("account.dat", ios::binary|ios::in|ios::out);
     bool found = false;
 
     while(!File.eof()) {
+        
         File.read(reinterpret_cast<char *> (&ac), sizeof(account));
+        
         if(ac.return_account() == n) {
 
             cout << "Enter Amount You Would Like To Withdraw: ";
             cin >> withdraw_amnt;
+            
             ac.withdraw(withdraw_amnt);
 
-            int pos=(-1)*static_cast<int>(sizeof(ac));
+            int pos = (-1)*static_cast<int>(sizeof(ac));
 			File.seekp(pos,ios::cur);       //sets the position where the next character is to be inserted to prevent copys.
+            
             found = true;
+            
             File.write(reinterpret_cast<char *> (&ac), sizeof(account));
             break;
         }
     }
 
     File.close();
-    if(found == false) {
+    
+    if(!found) {
         cout << "Record Does Not Exist... Press Any Key To Continue...";
     }
 }
 
 
-//4
-void display_balance(int n) {
+/***
+ * 04.
+ * Function used to display all information about the account required. 
+ * @show_account() function used from Account class.
+***/
+void display_actInfo(int n) {
     account ac;
     ifstream inFile;
+
 	inFile.open("account.dat",ios::binary);
     
     bool found = false;
@@ -256,7 +363,8 @@ void display_balance(int n) {
 
     cout << endl;
 
-    while(inFile.read(reinterpret_cast<char *> (&ac), sizeof(account)))  //reinterpret_cast<char *> (&ac) = treating &ac as a char 
+    //reinterpret_cast<char *> (&ac) = treating &ac as a char 
+    while(inFile.read(reinterpret_cast<char *> (&ac), sizeof(account)))  
     {
         if(ac.return_account() == n) {
             found = true;
@@ -272,25 +380,26 @@ void display_balance(int n) {
 
     cout << endl;
     cout << "Press Any Key To Continue...";
+
     inFile.close(); 
 }
 
-//5
-//6
+/***
+ * 05.
+ * Function used to close an account and remove all information about the account.
+***/
 void close_account() {
     account ac;
-    int b;
-    char c;
-
-    //bool confirm = false;
+    int id;
+    char confirm;
 
     cout << "Enter Account You Want To Delete: ";
-    cin >> b;
+    cin >> id;
 
     cout << "Are You Sure You Want To Delete Your Account? (y/n)" << endl;
-    cin >> c;
+    cin >> confirm;
 
-    if(c == 'n') {
+    if(confirm == 'n') {
         cout << "Account Will Not Be Deleted, Press Any Key To Return To Main Menu...";
         return;
     }
@@ -301,10 +410,11 @@ void close_account() {
     inFile.open("account.dat",ios::binary);
     newFile.open("temp.dat",ios::binary);
 
-    inFile.seekg(0,ios::beg);               //start at first pos.
+    //start at first pos.
+    inFile.seekg(0,ios::beg);             
     
     while(inFile.read(reinterpret_cast<char *> (&ac), sizeof(account))) {
-        if(ac.return_account() != b) {
+        if(ac.return_account() != id) {
             newFile.write(reinterpret_cast<char *> (&ac), sizeof(account));
         }
     }
@@ -318,39 +428,50 @@ void close_account() {
     cout << "Account Successfully Closed, Press Any Key To Return To Main Menu...";
 }
 
-//7
+/***
+ * 06.
+ * Function used to modify a account.
+ * Funcion calles @modify() from the account class.
+***/
 void modify_account() {
     account ac;
     fstream File;
-    int b;
+    int id;
 
     cout << "Enter The Account You Want To Modify: ";
-    cin >> b;
+    cin >> id;
 
 
     File.open("account.dat", ios::binary|ios::in|ios::out);
 
      while(!File.eof()) {
+
        File.read(reinterpret_cast<char *> (&ac), sizeof(account));
-       if(ac.return_account() == b) {
+
+       if(ac.return_account() == id) {
            ac.modify();
        }
        else {
            cout << "Account Does Not Exist, Press Any Key To Continue...";
        }
 
-        int pos=(-1)*static_cast<int>(sizeof(ac)); //...
-		File.seekp(pos,ios::cur);       //sets the position where the next character is to be inserted to prevent copys.
-        //found = true;
+        //sets the position where the next character is to be inserted to prevent copys.
+        int pos = (-1)*static_cast<int>(sizeof(ac));
+		File.seekp(pos,ios::cur);     
+       
         File.write(reinterpret_cast<char *> (&ac), sizeof(account));
+        
         break;
     }
 
     File.close();
-
 }
 
-//
+/***
+ * 07.
+ * Function used to display a list of all accounts in the database.
+ * Funcion calles @display_accounts() from the account class.
+***/
 void display_all() {
     account ac;
     ifstream File;
@@ -364,27 +485,20 @@ void display_all() {
     File.close();
     cout << "Press Any Key To Continue..." << endl;
 }
-void intro()
-{
-	cout << "\n\n\n\t  BANK";
-	cout << "\n\n\tMANAGEMENT";
-	cout << "\n\n\t  SYSTEM";
-	cout << "\n\n\n\nMADE BY : ASFAND KHAN";
-	cout << "\n\nPRESS ENTER TO CONTINUE: ";
-    cout << endl;
-	cin.get();
-}
 
+
+//-----------------------------------------------------------------------------------------------------------------------------
 int main() {
     //Main menu
 
     char ch;
-	int num;
+	int id;
     int account;
+
 	intro();
 
 	do {
-		//system("cls");
+
 		cout << "---------- MAIN MENU ----------" << endl;
         cout << "           01. NEW ACCOUNT" << endl;
         cout << "           02. DEPOSIT" << endl;
@@ -396,11 +510,9 @@ int main() {
         cout << "           08. EXIT" << endl;
         cout << "-------------------------------" << endl;
 
-        //User inputs
         cout << "Select Your Option <1-8>: ";  
         cin >> ch;
         
-		//system("cls");
 		switch(ch)
 		{
 		case '1':
@@ -408,19 +520,18 @@ int main() {
 			break;
         case '2': 
             cout << "\n\n\t Enter The account No. : ";
-            //cout << "\n\n\t Enter The Deposit Amount You Would Like To Make: ";
-            cin >> num;
-            deposit_amount(num);
+            cin >> id;
+            deposit_amount(id);
             break;
         case '3': 
             cout << "\n\n\t Enter The account No. : ";
-            cin >> num;
-            withdraw_amount(num);
+            cin >> id;
+            withdraw_amount(id);
             break;
         case '4': 
             cout << "\n\n\t Enter The account No. : ";
-            cin >> num;   //user enters account no.
-			display_balance(num);
+            cin >> id;   
+			display_actInfo(id);
 			break;
         case '5': 
             close_account();
